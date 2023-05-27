b7edef7a8e31357123e8a829e2a85c0c73e6b66d
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-05-27 19:05:59 +0500

    Welcome screen

---
+++ lib/pages/auth/views/login_view.dart
@@ -0,0 +1,14 @@
+import '../../../common/common_libs.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+
+      ),
+    );
+  }
+}
