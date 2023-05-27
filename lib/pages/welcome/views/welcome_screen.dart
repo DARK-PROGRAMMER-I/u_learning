e17295dcc7e21f49cd0f1cb8c313aa970cad585c
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-05-27 12:27:45 +0500

    Welcome screen

--- lib/pages/welcome/views/welcome_screen.dart
+++ lib/pages/welcome/views/welcome_screen.dart
@@ -11,9 +11,14 @@ import 'package:u_learning/utils/constants/font_manager.dart';
 
 import '../welcome_widgets/image_card.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
