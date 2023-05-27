4d9001b501533c7dc7346e516612f1b22dc84ad7
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-05-27 21:59:35 +0500

    Login Screen

--- lib/pages/auth/views/login_view.dart
+++ lib/pages/auth/views/login_view.dart
@@ -26,88 +26,102 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: 70.h,),
-          Text('Log In', style: getMediumStyle(fontSize: MyFonts.size16),),
-          SizedBox(height: 15.h,),
-          Divider(height: 1.h, color: MyColors.borderColor,),
-          SizedBox(height: 30.h,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(AppAssets.googleIcon, width: 34.w, height: 34.h,),
-              SizedBox(width: 30.w,),
-              Image.asset(AppAssets.appleIcon, width: 34.w, height: 34.h,),
-              SizedBox(width: 30.w,),
-              Image.asset(AppAssets.facebookIcon, width: 34.w, height: 34.h,),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 70.h,),
+            Text('Log In', style: getMediumStyle(fontSize: MyFonts.size16),),
+            SizedBox(height: 15.h,),
+            Divider(height: 1.h, color: MyColors.borderColor,),
+            SizedBox(height: 30.h,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                InkWell(
+                  borderRadius: BorderRadius.circular(30.r),
+                    onTap: (){},
+                    child: Image.asset(AppAssets.googleIcon, width: 34.w, height: 34.h,)),
+                SizedBox(width: 30.w,),
+                InkWell(
+                  borderRadius: BorderRadius.circular(30.r),
+                    onTap: (){},
+                    child: Image.asset(AppAssets.appleIcon, width: 34.w, height: 34.h,)),
+                SizedBox(width: 30.w,),
+                InkWell(
+                  borderRadius: BorderRadius.circular(30.r),
+                    onTap: (){},
+                    child: Image.asset(AppAssets.facebookIcon, width: 34.w, height: 34.h,)),
 
-            ],
-          ),
-          SizedBox(height: 20.h,),
-          Text('Or use your email account login.',
-            style: getRegularStyle(
-            color: MyColors.bluishGreyColor
-          ),
-          textAlign: TextAlign.center,
-          ),
-          SizedBox(height: 120.h,),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.w),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  CustomTextFieldNew(
-                      controller: _emailCtr,
-                      onChanged: (val){},
-                      onFieldSubmitted: (val){},
-                    iconData: Icons.person,
-                      labelText: 'Email '
-                  ),
-                  SizedBox(height: 10.h,),
-                  CustomTextFieldNew(
-                      controller: _emailCtr,
-                      onChanged: (val){},
-                      onFieldSubmitted: (val){},
-                      iconData: Icons.lock,
-                      labelText: 'Password '
-                  ),
-
-                  SizedBox(height: 20.h,),
-                  Text(
-                    'Forgot Password',
-                    style: TextStyle(
-                      fontSize: MyFonts.size14,
-                      decoration: TextDecoration.underline,
-                      fontWeight: FontWeightManager.semiBold,
-                      color: MyColors.themeColor
+              ],
+            ),
+            SizedBox(height: 20.h,),
+            Text('Or use your email account login.',
+              style: getRegularStyle(
+              color: MyColors.bluishGreyColor
+            ),
+            textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 120.h,),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.w),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    CustomTextFieldNew(
+                        controller: _emailCtr,
+                        onChanged: (val){},
+                        onFieldSubmitted: (val){},
+                      iconData: Icons.person,
+                        labelText: 'Email '
                     ),
-                  ),
+                    SizedBox(height: 10.h,),
+                    CustomTextFieldNew(
+                        controller: _emailCtr,
+                        onChanged: (val){},
+                        onFieldSubmitted: (val){},
+                        iconData: Icons.lock,
+                        labelText: 'Password '
+                    ),
+
+                    SizedBox(height: 20.h,),
+                    InkWell(
+                      onTap: (){},
+                      child: Text(
+                        'Forgot Password',
+                        style: TextStyle(
+                          fontSize: MyFonts.size14,
+                          decoration: TextDecoration.underline,
+                          fontWeight: FontWeightManager.semiBold,
+                          color: MyColors.themeColor
+                        ),
+                      ),
+                    ),
 
 
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          SizedBox(height: 70.h,),
-          CustomButton(
-            onPressed: (){},
-            buttonText: 'Log In',
-            buttonWidth: 330.w,
-            buttonHeight: 50.h,
-          ),
-          CustomButton(
-            onPressed: (){},
-            buttonText: 'Sign Up',
-            buttonWidth: 330.w,
-            buttonHeight: 50.h,
-            backColor: MyColors.white,
-            textColor: MyColors.blackColor,
-          ),
-        ],
+            SizedBox(height: 70.h,),
+            CustomButton(
+              onPressed: (){},
+              buttonText: 'Log In',
+              buttonWidth: 330.w,
+              buttonHeight: 50.h,
+            ),
+            CustomButton(
+              onPressed: (){},
+              buttonText: 'Sign Up',
+              buttonWidth: 330.w,
+              buttonHeight: 50.h,
+              backColor: MyColors.white,
+              textColor: MyColors.blackColor,
+            ),
+          ],
+        ),
       ),
     );
   }
