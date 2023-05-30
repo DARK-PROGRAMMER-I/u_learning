8efa62af79570e933293bf586fb92440e8617422
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-05-30 17:59:55 +0500

    Login Screen

--- lib/pages/auth/auth_blocs/auth_events.dart
+++ lib/pages/auth/auth_blocs/auth_events.dart
@@ -4,4 +4,10 @@ class AuthEvents{
 
 class AuthEmailEvent extends AuthEvents{
   final String email;
+  AuthEmailEvent({required this.email});
+}
+
+class AuthPasswordEvent extends AuthEvents{
+  final String password;
+  AuthPasswordEvent({required this.password});
 }
