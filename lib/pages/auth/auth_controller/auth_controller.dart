4db6f412f327759bdb7c0b8b4fde4456a4e2bbab
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-05-30 18:29:44 +0500

    Auth Controller

--- lib/pages/auth/auth_controller/auth_controller.dart
+++ lib/pages/auth/auth_controller/auth_controller.dart
@@ -3,4 +3,9 @@ import 'package:flutter/cupertino.dart';
 class AuthController{
   final BuildContext context;
   AuthController({required this.context});
+
+
+  Future onSignIn({required String loginType })async{
+
+  }
 }
