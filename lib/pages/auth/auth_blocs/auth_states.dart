8dc1587fe2a91df9bd7744df967a078de67befe1
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-05-30 18:08:05 +0500

    Login Screen

--- lib/pages/auth/auth_blocs/auth_states.dart
+++ lib/pages/auth/auth_blocs/auth_states.dart
@@ -2,9 +2,12 @@ class AuthStates{
   final String email;
   final String password;
 
-  AuthStates({this.email = '',this.password,});
+  AuthStates({this.email = '',this.password = '',});
 
-  AuthStates.copyWith(this.email, this.password){
-    AuthStates(email: email, password: password);
+  AuthStates copyWith({String? email, String? password}){
+    return AuthStates(
+        email: email ?? this.email,
+        password: password ?? this.password,
+    );
   }
 }
