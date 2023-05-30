22643443503791a2e1617d6658dc32d932db337a
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-05-30 19:04:56 +0500

    Auth Controller

--- lib/pages/auth/auth_controller/auth_controller.dart
+++ lib/pages/auth/auth_controller/auth_controller.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:u_learning/common/enums/category_type.dart';
+import 'package:u_learning/pages/auth/auth_blocs/auth_bloc.dart';
+import 'package:u_learning/pages/auth/auth_blocs/auth_states.dart';
 
 class AuthController{
   final BuildContext context;
   AuthController({required this.context});
 
 
-  Future onSignIn({required AuthType loginType })async{
+  Future handleSignIn({required AuthType loginType })async{
     if(loginType.name.toAuthTypeEnum() == AuthType.email){
+      final state = context.read<AuthBlocs>().state;
+      if(state.email == null){
 
+      }else if()
     }else{
 
     }
