7460e9f1f84ddfe1bffedfbb1eb3ba09f4d87ace
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-05-31 14:52:43 +0500

    Auth Controller

--- lib/pages/auth/auth_controller/auth_controller.dart
+++ lib/pages/auth/auth_controller/auth_controller.dart
@@ -12,27 +12,30 @@ class AuthController{
 
   Future handleSignIn({required AuthType loginType })async{
     try{
+      if(loginType.name.toAuthTypeEnum() == AuthType.email){
+        final state = context.read<AuthBlocs>().state;
+        if(state.email == ''){
 
-    }catch
-    if(loginType.name.toAuthTypeEnum() == AuthType.email){
-      final state = context.read<AuthBlocs>().state;
-      if(state.email == ''){
+        }else if(state.password == ''){
 
-      }else if(state.password == ''){
+        }
+
+        final String email = state.email;
+        final String password = state.password;
+
+        try{
+
+        }on FirebaseAuthException catch (e, st){
+          debugPrintStack(stackTrace: st);
+          debugPrint(e.toString());
+        }
+      }else{
 
       }
-
-      final String email = state.email;
-      final String password = state.password;
-
-      try{
-
-      }on FirebaseAuthException catch (e, st){
-        debugPrintStack(stackTrace: st);
-        debugPrint(e.toString());
-      }
-    }else{
-
+    }catch(e, st){
+      debugPrintStack(stackTrace: st);
+      debugPrint(e.toString());
     }
+
   }
 }
