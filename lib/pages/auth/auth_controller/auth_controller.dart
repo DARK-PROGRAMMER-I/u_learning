34f3bd3a9aeaea6418fe616fd9e0297d2a38f395
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-05-31 22:35:09 +0500

    Auth Controller

--- lib/pages/auth/auth_controller/auth_controller.dart
+++ lib/pages/auth/auth_controller/auth_controller.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:u_learning/common/common_widgets/show_toast.dart';
 import 'package:u_learning/common/enums/category_type.dart';
 import 'package:u_learning/pages/auth/auth_blocs/auth_bloc.dart';
 import 'package:u_learning/pages/auth/auth_blocs/auth_states.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:u_learning/pages/auth/auth_errors.dart';
 
 class AuthController{
   final BuildContext context;
@@ -35,7 +37,8 @@ class AuthController{
 
         }on FirebaseAuthException catch (e, st){
           debugPrintStack(stackTrace: st);
-          debugPrint(e.toString());
+          AuthErrors error = AuthErrors.from(e);
+          showSnackBar(context, '${error.dialogeTitle} \n ${error.dialogeText}');
         }
       }else{
 
