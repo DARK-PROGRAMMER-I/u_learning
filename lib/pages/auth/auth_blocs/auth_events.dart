fb4fbbd81ab3f8e3254a931700fff4c6d20653bb
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-06-01 20:35:54 +0500

    Register blocs

--- lib/pages/auth/auth_blocs/auth_events.dart
+++ lib/pages/auth/auth_blocs/auth_events.dart
@@ -2,10 +2,16 @@ class AuthEvents{
   const AuthEvents();
 }
 
+class AuthNameEvent extends AuthEvents{
+  final String name;
+  AuthNameEvent({required this.name});
+}
+
 class AuthEmailEvent extends AuthEvents{
   final String email;
   AuthEmailEvent({required this.email});
 }
+
 
 class AuthPasswordEvent extends AuthEvents{
   final String password;
