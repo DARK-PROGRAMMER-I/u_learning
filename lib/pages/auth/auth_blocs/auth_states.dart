d4aaddbf5957f90e305bd06291ac4cf91efa592f
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-06-01 20:35:13 +0500

    Register blocs

--- lib/pages/auth/auth_blocs/auth_states.dart
+++ lib/pages/auth/auth_blocs/auth_states.dart
@@ -1,13 +1,17 @@
 class AuthStates{
+  final String name;
   final String email;
   final String password;
+  final String cnfrmPassword;
 
-  AuthStates({this.email = '',this.password = '',});
+  AuthStates({this.email = '',this.password = '', this.name= '', this.cnfrmPassword= '',  });
 
-  AuthStates copyWith({String? email, String? password}){
+  AuthStates copyWith({String? name,String? email,String? cnfrmPassword, String? password}){
     return AuthStates(
         email: email ?? this.email,
         password: password ?? this.password,
+        name: name ?? this.name,
+        cnfrmPassword: cnfrmPassword ?? this.cnfrmPassword,
     );
   }
 }
