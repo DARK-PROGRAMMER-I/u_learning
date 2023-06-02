0bb0c1daac25d9202782a14f87a925736ac76609
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-06-02 18:22:33 +0500

    Register blocs

--- lib/pages/home/views/home_screen.dart
+++ lib/pages/home/views/home_screen.dart
@@ -17,47 +17,6 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<HomeBlocs, HomeStates>(
-      builder: (BuildContext context, state) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('Home Screen'),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'You have pushed the button this many times:',
-                ),
-                Text(
-                  '${state.counter}',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ],
-            ),
-          ),
-
-          bottomNavigationBar:  Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              FloatingActionButton(
-                heroTag: 'hola-kapito',
-                onPressed: (){BlocProvider.of<HomeBlocs>(context).add(Increment());},
-                tooltip: 'Increment',
-                child: const Icon(Icons.add),
-              ),
-              FloatingActionButton(
-                heroTag: 'hola',
-                onPressed: (){BlocProvider.of<HomeBlocs>(context).add(Decrement());},
-                tooltip: 'Decrement',
-                child: const Icon(Icons.remove),
-              ),
-            ],
-          ),
-        );
-      },
-
-    );
+    return Scaffold();
   }
 }
