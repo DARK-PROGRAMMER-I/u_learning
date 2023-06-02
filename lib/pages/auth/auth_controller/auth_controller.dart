7a77966a37b6db105affe260e9c55bf172941c3e
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-06-02 15:04:44 +0500

    Register blocs

--- lib/pages/auth/auth_controller/auth_controller.dart
+++ lib/pages/auth/auth_controller/auth_controller.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:u_learning/common/common_widgets/show_toast.dart';
 import 'package:u_learning/common/enums/category_type.dart';
 import 'package:u_learning/pages/auth/auth_blocs/auth_bloc.dart';
-import 'package:u_learning/pages/auth/auth_blocs/auth_states.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:u_learning/pages/auth/auth_errors.dart';
 
@@ -13,6 +12,43 @@ class AuthController{
 
 
   Future handleSignIn({required AuthType loginType })async{
+    try{
+      if(loginType.name.toAuthTypeEnum() == AuthType.email){
+        final state = context.read<AuthBlocs>().state;
+        if(state.email == ''){
+
+        }else if(state.password == ''){
+
+        }
+
+        final String email = state.email;
+        final String password = state.password;
+
+        try{
+          final credentials  = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+          if(credentials.user == null){
+
+          }
+
+          if(!credentials.user!.emailVerified){
+
+          }
+
+        }on FirebaseAuthException catch (e, st){
+          AuthErrors error = AuthErrors.from(e);
+          showSnackBar(context, '${error.dialogeTitle} \n ${error.dialogeText}');
+        }
+      }else{
+
+      }
+    }catch(e, st){
+      debugPrintStack(stackTrace: st);
+      debugPrint(e.toString());
+    }
+
+  }
+
+  Future handleRegister({required AuthType loginType })async{
     try{
       if(loginType.name.toAuthTypeEnum() == AuthType.email){
         final state = context.read<AuthBlocs>().state;
