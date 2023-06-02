1bf1a6ed3258d31070be92b8284cb3fbbd788e46
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2023-06-02 18:24:49 +0500

    Register blocs

--- lib/common/common_blocs/common_blocs.dart
+++ lib/common/common_blocs/common_blocs.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../../pages/auth/auth_blocs/auth_bloc.dart';
 import '../../pages/home/home_blocs/home_blocs.dart';
 import '../../pages/welcome/welcome_blocs/welcome_bloc.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:u_learning/common/common_blocs/common_blocs.dart';
 import 'package:u_learning/firebase_options.dart';
 import 'package:u_learning/pages/auth/auth_blocs/auth_bloc.dart';
 import 'package:u_learning/pages/home/home_blocs/home_blocs.dart';
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
       splitScreenMode: true,
       builder: (context, child) {
         return MultiBlocProvider(
-          providers:
+          providers: AppBlocs.allBlocs,
 
           child: MaterialApp(
             title: 'Flutter Demo',
